672510a8673ff72417fb6bfb0747600927088884
Brendan McDonald <brendan@brendandmcdonald.com>
2015-04-28 13:31:41 +0100

    Initial commit

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        var label = UILabel()
+        label.frame = CGRectMake(139, 185, 60, 60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRectMake(224, 185, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.purpleColor(), forState: .Normal)
+        
+        self.view.addSubview(button)
+        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
     }
-
+    
+    
 
 }
 
